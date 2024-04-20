009bf4db908d736414349aa28c42027057f7b519
POJO <2917967684@qq.com>
2024-04-20 14:04:57 +0800

    first commit

--- admin-api/src/main/java/com/briup/bookstore/web/controller/AdminController.java
+++ admin-api/src/main/java/com/briup/bookstore/web/controller/AdminController.java
@@ -13,7 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.UUID;
-
+//第一次改变
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
